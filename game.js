e9fcfc61a7232522da3a467940af0bb6911e063d
voidovertwo <halfvoid@gmail.com>
2025-12-16 21:27:32 +0000

    Update game.js

--- game.js
+++ game.js
@@ -289,7 +289,7 @@ class MapSegment {
 
 class GameState {
     constructor() {
-        this.globalZP = 1000;
+        this.globalZP = 10000;
         this.runnersSentCount = 0;
         this.runners = [];
         this.relics = {};
